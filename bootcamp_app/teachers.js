662eb719d312c9dd4daba574498f74d6ade181f0
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-04-06 18:32:21 -0400

    bootcamp app injection-safe

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const queryString = `SELECT DISTINCT teachers.name as name, cohorts.name as cohort
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN teachers ON teachers.id = teacher_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1;`
 
-pool.query(`
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort 
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id 
-  JOIN students ON student_id = students.id
-  JOIN cohorts  ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-  ORDER BY teacher;
-`)
+const cohortName = process.argv[2] || 'JUL02';
+
+const values = [`%${cohortName}%`];
+
+
+pool.query(queryString, values)
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+    console.log(res.rows);
   })
-}).catch(err => console.error('query error', err.stack));
-
-; 
+.catch(err => console.error('query error', err.stack));
